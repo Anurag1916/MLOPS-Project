40a2805c12cb03d661eeaa3cf3526fffcc3dd134
Anurag1916 <anurag.shrivastava370@gmail.com>
2025-04-20 08:31:38 +0530

    project structure created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anurag Shrivastava",
+    author_email="anurag.shrivastava370@gmail.com",
+    packages=find_packages()
+)
